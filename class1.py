99cda4e99d07597385ae66e4068df7c58edd6376
Dinesh7574 <123284816+Dinesh7574@users.noreply.github.com>
2023-02-04 15:55:58 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[2]:
+
+
+print("hi")
+print("hello")
+print("welcome")
+
+
+# In[3]:
+
+
+print("hi\n hello\n welcome \n")
+
+
+# In[5]:
+
+
+# it is a command (backcase)
+print("hi")
+#print("hello")
+print("welcome")
+
+
+# In[7]:
+
+
+"""
+
+dkdkl
+dkdk
+
+
+
+
+"""
+
+
+# In[8]:
+
+
+input("enter a value")
+
+
+# In[9]:
+
+
+print("welcome to class")
+
+
+# In[10]:
+
+
+a="welcome to class"
+
+
+# In[11]:
+
+
+# variable name should not have a spl char
+#variable name shld not start with number
+print (a)
+
+
+# In[13]:
+
+
+a = "test"
+print(type(a))
+
+
+# In[14]:
+
+
+print(isinstance(a,str))
+
+
+# In[19]:
+
+
+a = "SachinTendulkar"
+print(len(a))
+print(max(a))
+print(min(a))
+print(sorted(a))
+
+
+# In[20]:
+
+
+print(a.upper())
+print(a.lower())
+print(a.swapcase())
+print(a.islower())
+print(a.isupper())
+print(a.isdigit())
+
+
+# In[21]:
+
+
+print(a[5])
+
+
+# In[22]:
+
+
+#start : end : interval
+print(a[2:10])  #python 0 >1>2>3> end word -3<-2<-1
+print(a[ : :])
+print(a[2:15:3])
+print(a[ : :-1])#reverse the python string
+
+
+# In[23]:
+
+
+print(a.index("a"))
+print(a.index("a",5))
+print(a.rindex("a")) #reverse index
+
+
+# In[25]:
+
+
+print(a.find("z")) #find and index are same but if the value is not present -1 is shown
+print(a.rfind("a"))
+
+
+# In[26]:
+
+
+print(a.count("a"))
+print(a.split("a"))
+print(a.replace("a","$"))
+
+
+# In[8]:
+
+
+a = input()
+print(max(a))
+print(min(a))
+print(sorted(a))
+print(a.isdigit())
+print(a.index("a"))
+
+
+# In[9]:
+
+
+
+
+
+# In[10]:
+
+
+print(a.isalnum()) # it is to check alphabet or number
+
+
+# In[11]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
